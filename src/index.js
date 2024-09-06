0c15176678eea3beb27a7334f73a837afa9040c8
AnnaSergeevnaIv <kara4eva.anya@gmail.com>
2024-09-06 11:16:56 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,25 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let result = ''
+    for (let i = 0; i < expr.length / 10; i++) {
+        let letter = expr.slice(i * 10, i * 10 + 10)
+        if (letter === '**********') {
+            result += ' '
+            continue
+        }
+        let subresult = ''
+        for (let j = 0; j < letter.length / 2; j++) {
+            switch (letter[j*2] + letter[j*2+1]) {
+                case '00' : break;
+                case '10' : subresult += '.'; break;
+                case '11' : subresult += '-'; break;
+            }
+        }
+        result += MORSE_TABLE[subresult]
+    }
+
+    return result
 }
 
 module.exports = {
